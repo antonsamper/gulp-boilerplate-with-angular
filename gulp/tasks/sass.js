eb05aad27b92e842b538f97d42873cd16cb942d3
Anton Samper Rivaya <anton@aeser.co.uk>
2015-03-09 19:24:24 +0000

    refactor(gulp): separating `sass` task

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,43 @@
+/*
+ * @title Sass
+ * @description A task to compile Sass to CSS
+ * @example (cli) gulp sass
+ */
+
+
+/*********************************************************************************
+ 1. DEPENDENCIES
+ *********************************************************************************/
+
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var csso = require('gulp-csso');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var plumber = require('gulp-plumber');
+var rev = require('gulp-rev');
+var sass = require('gulp-sass');
+var sharedPaths = require('../shared/paths.js');
+var sharedEvents = require('../shared/events.js');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+/*********************************************************************************
+ 2. TASK
+ *********************************************************************************/
+
+gulp.task('sass', function () {
+  return gulp
+    .src(sharedPaths.srcDir + '/sass/*.scss')
+    .pipe(plumber({
+      errorHandler: sharedEvents.onError
+    }))
+    .pipe(gulpif(process.env.ENVIRONMENT_TYPE === 'dev', sourcemaps.init()))
+    .pipe(sass())
+    .pipe(autoprefixer({browsers: ['last 2 version']}))
+    .pipe(gulpif(process.env.ENVIRONMENT_TYPE === 'dev', sourcemaps.write()))
+    .pipe(gulpif(process.env.ENVIRONMENT_TYPE !== 'dev', csso()))
+    .pipe(gulpif(process.env.ENVIRONMENT_TYPE !== 'dev', rev()))
+    .pipe(gulp.dest(sharedPaths.outputDir + '/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
