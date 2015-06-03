a0e717f0c3b8baa2904848163bf47573f7f9c0ed
Anton Samper Rivaya <anton@aeser.co.uk>
2015-06-03 07:35:17 +0100

    refactor(app.js): updating syntax

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,28 @@
+/*
+ * @title app
+ * @description Module definition for the app
+ */
+
+
+/*********************************************************************************
+ 1. MODULE DEFINITION
+ *********************************************************************************/
+
 (function (app) {
 
+  var moduleName = 'app';
+  var dependencies = ['iconButton'];
+
+  try {
+    app = angular.module(moduleName);
+  } catch (e) {
+    app = angular.module(moduleName, dependencies);
+  }
+
   function ExampleCtrl() {
-
     this.use = 'AngularJS SPAs!';
-
   }
 
   app.controller('ExampleCtrl', ExampleCtrl);
 
-})(angular.module('app', ['iconButton']));
+})();
