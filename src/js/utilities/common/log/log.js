baf9b9b9430f736620d064b9197c64b884793b51
Anton Samper Rivaya <anton@aeser.co.uk>
2015-08-26 19:02:30 +0100

    chore(utilities): adding log utility

---
+++ src/js/utilities/common/log/log.js
@@ -0,0 +1,25 @@
+/*
+ * @title appLog
+ * @description Module definition for the log utility
+ */
+
+
+/*********************************************************************************
+ 1. UTILITY DEFINITION
+ *********************************************************************************/
+
+var commonUtilities = commonUtilities || {};
+
+commonUtilities.log = {
+  colors: {
+    blue:   'font-weight: bold; color: #6699CC;',
+    brown:  'font-weight: bold; color: #AB7967;',
+    gray:   'font-weight: bold; color: #1B2B34;',
+    green:  'font-weight: bold; color: #99C794;',
+    orange: 'font-weight: bold; color: #F99157;',
+    purple: 'font-weight: bold; color: #C594C5;',
+    red:    'font-weight: bold; color: #EC5f67;',
+    teal:   'font-weight: bold; color: #5FB3B3;',
+    yellow: 'font-weight: bold; color: #FAC863;'
+  }
+};
